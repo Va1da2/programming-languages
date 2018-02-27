938bb14f1dcae68919b80c78e902a53bf23e7cdb
Vaidas <vaidas.armonas@gmail.com>
2018-02-27 08:37:26 +0200

    Racket intro

---
+++ part-b/section5/racket_intro.rkt
@@ -0,0 +1,11 @@
+; Programming Languages: Section 5 - Racket Introduction
+
+; Always make this the first (non-comment, non-blank) line of your script
+#lang racket
+
+; not needed here, but a workaround so we could write tests in a second file
+; see getting-started-with-Racket instructions for more explanation
+(provide (all-defined-out))
+
+; basic definitions
+(define s "hello")
