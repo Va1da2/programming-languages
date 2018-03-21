8c28f57a24632907a34b7147fb487736cb57c662
Vaidas <vaidas.armonas@gmail.com>
2018-03-21 08:42:35 +0200

    extending programs in fp vs oop

---
+++ part-c/section9/adding_operations_or_variants.rb
@@ -0,0 +1,109 @@
+# Programming Languages: Section 9 - Adding Operations or Variants
+
+class Exp
+    # could put default implementation or helper methods here
+end
+
+class Value < Exp
+    # Overkill here but usefull if you have multiple kinds of
+    # /values/ in your language that can share methods that
+    # do not make sense for non value expressions
+end
+
+class Int < Value
+    attr_reader: :i
+    def initialize i
+        @i = i
+    end
+
+    def eval # no arguments because no environment
+        self
+    end
+
+    def toString
+        @i.to_s
+    end
+
+    def hasZero
+        i == 0
+    end
+
+    def noNegConstant
+        if i < 0
+            Negate.new(Int.new(-i))
+        else
+            self
+        end
+    end
+end
+
+class Negate < Exp
+    attr_reader :e
+    def initialize e
+        @e = e
+    end
+
+    def eval
+        Int.new(-e.eval.i) # error if `e.eval` has no `i` method
+    end
+
+    def toString
+        "-(" + e.toString + ")"
+    end
+
+    def hasZero
+        e.hasZero
+    end
+
+    def noNegConstants
+        Negate.new(e.noNegConstants)
+    end
+end
+
+class Add < Exp
+    attr_reader :e1, :e2
+    def initialize(e1, e2)
+        @e1 = e1
+        @e2 = e2
+    end
+
+    def eval
+        Int.new(e1.eval.i + e2.eval.i) # error if `e1.eval` or `e2.eval` has no `i` method
+    end
+
+    def toString
+        "(" + e1.toString + " + " + e2.toString + ")"
+    end
+
+    def hasZero
+        e1.hasZero || e2.hasZero
+    end
+
+    def noNegConstants
+        Add.new(e1.noNegConstants, e2.noNegConstants)
+    end
+end
+
+class Mult < Exp
+    attr_reader :e1, :e2
+    def initialize(e1, e2)
+        @e1 = e1
+        @e2 = e2
+    end
+
+    def eval
+        Int.new(e1.eval.i * e2.eval.i)
+    end
+
+    def toString
+        "(" + e1.toString + " * " + e2.toString + ")"
+    end
+
+    def hasZero
+        e1.hasZero || e2.hasZero
+    end
+
+    def noNegConstants
+        Mult.new(e1.noNegConstants, e2.noNegConstants)
+    end
+end
---
+++ part-c/section9/adding_operations_or_variants.sml
@@ -0,0 +1,53 @@
+(* Programming Languages: Section 9 - OOP vs. Functional Decomposition *)
+
+datatype exp =
+	 Int of int
+	 | Negate of exp
+	 | Add of exp * exp
+	 | Mult of exp * exp;
+
+exception BadResult of string;
+
+(* helper function is overkill here but wil provide a more direct contrast
+   with more complicated examples soon *)
+fun add_values (v1, v2) =
+    case (v1, v2) of
+	(Int i, Int j) => Int (i + j)
+      | _ => raise BadResult "non-ints in addition";
+
+fun multiply_values (v1, v2) =
+    case (v1, v2) of
+	(Int i, Int j) => Int (i * j)
+      | _ => raise BadResult "non-ints in multiplication";
+
+fun eval e = (* no environment because no variable exps *)
+    case e of
+	Int _ => e
+      | Negate e1 => (case eval e1 of
+			  Int i => Int (~i)
+			| _ => raise BadResult "non-int in negation")
+      | Add(e1, e2) => add_values (eval e1, eval e2)
+      | Mult(e1, e2) => multiply_values (eval e1, eval e2);
+
+fun toString e =
+    case e of
+	Int i => Int.toString i
+      | Negate e1 => "-(" ^ (toString e1) ^ ")"
+      | Add(e1, e2) => "(" ^ (toString e1) ^ " + " ^ (toString e2) ^ ")"
+      | Mult(e1, e2) => "(" ^ (toString e1) ^ " * " ^ (toString e2) ^ ")";
+
+fun hasZero e =
+    case e of
+	Int i => i=0
+      | Negate e1 => hasZero e1
+      | Add(e1, e2) => (hasZero e1) orelse (hasZero e2)
+      | Mult(e1, e2) => (hasZero e1) orelse (hasZero e2);
+
+fun noNegConstants e =
+    case e of
+	Int i => if i < 0 then Negate (Int (~i)) else e
+      | Negate e1 => Negate (noNegConstants e1)
+      | Add(e1, e2) => Add (noNegConstants e1, noNegConstants e2)
+      | Mult(e1, e2) => Mult (noNegConstants e1, noNegConstants e2);
+
+				  
